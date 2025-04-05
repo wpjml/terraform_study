e8de62907768496206a175efa75c1bc78726c371
wpjml <wpjml.030927@gmail.com>
2025-04-05 21:19:47 +0900

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_version = ">= 1.1.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
+
+resource "aws_s3_bucket" "practice_bucket" {
+  bucket = var.bucket_name
+  acl    = "private"
+
+  tags = {
+    Name        = var.bucket_name
+    Environment = var.environment
+  }
+}
